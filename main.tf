682ab6b6667eb174b6d363f4014652d83ecc3253
Yash Khemani <yash@khemani.com>
2022-04-13 14:02:48 +0000

    fix vpc module

--- main.tf
+++ main.tf
@@ -67,9 +67,9 @@ resource "google_compute_instance" "hashicat" {
   tags = ["http-server"]
 
   labels = {
-    name = "hashicat"
+    name       = "hashicat"
     department = "devops"
-    billable = "true"
+    billable   = "true"
   }
 
 }
--- remote_backend.tf
+++ remote_backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "remote" {
-    hostname = "app.terraform.io"
+    hostname     = "app.terraform.io"
     organization = "khemani-training"
     workspaces {
       name = "hashicat-gcp"
